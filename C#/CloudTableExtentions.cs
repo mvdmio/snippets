76a161a54cc972e44735df227ae248694628ebf0
Michiel van der Meer <michielmeer1990@hotmail.com>
2018-10-12 08:17:48 +0200

    Added CloudTableExtention class for C#

---
+++ C#/CloudTableExtentions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace MMBusiness.Data.Azure
+{
+    /// <summary>
+    /// Additions and simplification of CloudTable methods.
+    /// </summary>
+    public static class CloudTableExtentions
+    {
+        /// <summary>
+        /// Execute the query on the table and return the results as an IEnumerable.
+        /// </summary>
+        /// <typeparam name="T">The type of result.</typeparam>
+        /// <param name="cloudTable">The table against which to run the query.</param>
+        /// <param name="query">The query.</param>
+        /// <returns>An array of results as returned by executing the query.</returns>
+        /// <remarks>
+        /// .NET Core 2.0 cloudtables are missing a simple ExecuteQuery method.
+        /// Instead, we need to do a rolling query using a continuation token.
+        /// This method encapulates that behaviour, so that users don't have to worry about this anymore.
+        /// </remarks>
+        public static async Task<IEnumerable<T>> ExecuteQueryAsync<T>(this CloudTable cloudTable, TableQuery<T> query) where T : ITableEntity, new()
+        {
+            TableContinuationToken continuationToken = null;
+            var result = new List<T>();
+            do
+            {
+                var iterationResult = await cloudTable.ExecuteQuerySegmentedAsync(query, continuationToken);
+                continuationToken = iterationResult.ContinuationToken;
+                result.AddRange(iterationResult.Results);
+            }
+            while (continuationToken != null);
+
+            return result;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# snippets
+# Snippets
+
+
+This repository contains snippets for various languages that I've accumulated over the years.
+You are free to use any and all snippets for your own use.
+Attribution is not required, but I do appreciate it.
